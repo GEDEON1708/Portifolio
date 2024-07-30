51172430fd6ffc9c9ce04cb0dde9b441f629565d
GEDEON KASHOMONA <kalala.gedeon88@gmail.com>
2024-07-30 19:20:31 -0300

    Atualização do Footer resolvendo problema com react-icons

--- src/components/Sections/Presentation.js
+++ src/components/Sections/Presentation.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import styles from './Presentation.module.css'
 import Button1 from '../elements/Button1'
-import perfil from '../../Image/projects/perfil.jpg'
 
 
 
@@ -11,11 +10,8 @@ function Presentation(){
             <h4><strong> Bem-vindo ao meu Portfólio </strong></h4>
             <h1> Olá, eu sou Gedeon! </h1>
             <p>
-                Sou um apaixonado por tecnologia e soluções inovadoras.<br></br>
-                Como Product Manager, eu tenho o compromisso de resolver problemas<br></br>
-                complexos e trazer resultados excepcionais para os negócios.<br></br>
-                Meus projetos já geraram milhões de reais em receita anual <br></br>
-                estou sempre em busca de novos desafios para superar.
+                Sou estudante de Ciência e Tecnologia na Universidade Federal do Rio Grande do Norte,<br></br>
+                focado em programação frontend. Aspiro me tornar um programador fullstack, sempre<br></br>buscando conhecimento e novos desafios. Além do curso superior, estudo constantemente<br></br> em plataformas de programação. Minha paixão por tecnologia e soluções inovadoras me<br></br> impulsiona a crescer e contribuir significativamente em projetos de desenvolvimento.
             </p>
 
             <Button1 link='https://github.com/GEDEON1708' text='Conecte-se comigo!'/>
