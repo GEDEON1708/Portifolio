92f3bac02929bd0dd93dd838a701c5ac9042d7e0
GEDEON KASHOMONA <kalala.gedeon88@gmail.com>
2024-07-30 20:18:02 -0300

    atualização do icon do email

--- src/components/Sections/Footer.js
+++ src/components/Sections/Footer.js
@@ -1,4 +1,4 @@
-import { FaInstagram, FaLinkedinIn, FaGithub, FaWhatsapp } from "react-icons/fa";
+import { FaInstagram, FaLinkedinIn, FaGithub, FaWhatsapp, FaEnvelope } from "react-icons/fa";
 import styles from './Footer.module.css'
 
 
@@ -10,9 +10,9 @@ function Footer(){
                 <li><a href='https://github.com/GEDEON1708'> <FaGithub size={30}/></a></li>
                 <li><a href='https://www.linkedin.com/in/gedeon-kashomona-5490472b6/'> <FaLinkedinIn size={30}/></a></li>
                 <li><a href='https://wa.me/5584998685592'><FaWhatsapp size={30}/></a></li>
+                <li><a href="mailto:gedeonkashomona@outlook.com" className={styles.email}><FaEnvelope size={30}/></a></li>
                 
             </ul>
-            <p>gedeonkashomona@outlook.com</p>
         </div>
     )
 }
